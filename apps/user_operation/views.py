909eb5b9ef9291b12a1030f19d6329655d562e7b
unknown <499428970@qq.com>
2021-06-16 23:09:04 +0800

    drf的权限验证

--- apps/user_operation/views.py
+++ apps/user_operation/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.authentication import SessionAuthentication
+
 from .models import UserFav
+from utils.permissions import IsOwnerOrReadOnly
 from .serializers import UserFavSerializer
 
 
 # Create your views here.
 
 class UserFavViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
-    queryset = UserFav.objects.all()
+    """
+    用户收藏功能
+    """
+    # queryset = UserFav.objects.all()
+    # 判断是否登录，未登录无法获取数据
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
     serializer_class = UserFavSerializer
+    authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)
+
+    def get_queryset(self):
+        return UserFav.objects.filter(user=self.request.user)
